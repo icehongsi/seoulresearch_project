29710663aeaf56463d06ae237066876c4ef8d2d6
icehongsi <52072412+icehongsi@users.noreply.github.com>
2020-08-19 15:44:24 +0900

    Update juso_api.py

--- Research/juso_api.py
+++ Research/juso_api.py
@@ -1,6 +1,17 @@
 import requests
 
-url =  "http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=10&keyword=서울특별시 종로구 동숭동 4'&confmKey=devU01TX0FVVEgyMDIwMDgxMTAwMzYxMjExMDA0NTY=&resultType=json"
+url =  "http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=1&keyword={}&confmKey=devU01TX0FVVEgyMDIwMDgxMTAwMzYxMjExMDA0NTY=&resultType=json"
 
-result = requests.get(url)
-print(result.json())
+
+
+def doro_to_jibun(address):
+    try:
+        result = requests.get(url.format(address)).json()
+        print(result["results"]["juso"][0]["jibunAddr"])
+    except:
+        print("No Result")
+        pass
+
+while True:
+    temp = input()
+    doro_to_jibun(temp)
